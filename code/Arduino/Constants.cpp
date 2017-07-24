222cddc72152ec2e5c1037f8a0c64eac7f380877
AJIOB <aleksandr9809@gmail.com>
2017-07-24 23:47:03 +0300

    Константы, которые работают на роботе

--- code/Arduino/Constants.cpp
+++ code/Arduino/Constants.cpp
@@ -1,41 +1,44 @@
 #include "Constants.h"
 
+/* Bluetooth */
+const uint8_t Constants::bluetooth_RX = 50;
+const uint8_t Constants::bluetooth_TX = 51;
+const uint32_t Constants::bluetooth_serial_speed = 9600;
+
+/* Wi-fi */
+const uint8_t Constants::wifi_RX = 30;
+const uint8_t Constants::wifi_TX = 31;
+const uint32_t Constants::wifi_serial_speed = 9600;
+
+/* Movement controller (L298N) */
+const uint8_t Constants::left_engine_enable = A8;
+const uint8_t Constants::left_engine_straight_pin = 30;
+const uint8_t Constants::left_engine_reverse_pin = 31;
+const uint8_t Constants::right_engine_straight_pin = 32;
+const uint8_t Constants::right_engine_reverse_pin = 33;
+const uint8_t Constants::right_engine_enable = A9;
+
+/* Distanse controller */
+const uint8_t Constants::distance_sensor_read_pin = A0;
+const uint8_t Constants::distance_sensor_a_pin = 4;
+const uint8_t Constants::distance_sensor_b_pin = 5;
+const uint8_t Constants::distance_sensor_c_pin = 6;
+
+/* Line controller */
+const uint8_t Constants::line_sensor_read_pin = A1;
+const uint8_t Constants::line_sensor_a_pin = 15;
+const uint8_t Constants::line_sensor_b_pin = 16;
+const uint8_t Constants::line_sensor_c_pin = 17;
+
+const uint8_t Constants::servo_horizontal_pin = 34;
+const uint8_t Constants::servo_vertical_pin = 35;
+
+const char Constants::commands_delimetr = ';';
+
 Constants::Constants()
 {
-	bluetooth_RX = 50;
-	bluetooth_TX = 51;
-	bluetooth_serial_speed = 9600;
-
-	wifi_RX = 30;
-	wifi_TX = 31;
-	wifi_serial_speed = 9600;
-
-	left_engine_enable = 7;
-	left_engine_straight_pin = 8;
-	left_engine_reverse_pin = 9;
-	right_engine_straight_pin = 10;
-	right_engine_reverse_pin = 11;
-	right_engine_enable = 12;
-
-	/*distance_sensor_read_pin = A0;*/
-	distance_sensor_a_pin = 4;
-	distance_sensor_b_pin = 5;
-	distance_sensor_c_pin = 6;
-
-	//line_sensor_read_pin = A1;
-	line_sensor_a_pin = 15;
-	line_sensor_b_pin = 16;
-	line_sensor_c_pin = 17;
-
-	servo_horizontal_pin = 34;
-	servo_vertical_pin = 35;
-
-	commands_delimetr = ';';
 }
-
 
 Constants::~Constants()
 {
 }
-
-
