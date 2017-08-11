a596407d34ed1f27144b97f13ca74843b00a9058
Fiery Bird <lex150498@mail.ru>
2017-08-11 20:58:15 +0300

    Прописан частичный функционал модуля

--- code/Arduino/connectors/WiFi.cpp
+++ code/Arduino/connectors/WiFi.cpp
@@ -4,9 +4,109 @@
 
 WiFi::WiFi(int rx, int tx, int speed) : ConnectingDevice(rx, tx, speed)
 {
+	Check();
+	ChangeSpeed(speed);
 }
-
 
 WiFi::~WiFi()
 {
 }
+
+String WiFi::VersionCheck()
+{
+	send("AT+GMR");
+	return read();
+}
+
+String CheckIPandMAC()
+{
+	send("AT+CIFSR");
+	return read();
+}
+
+void WiFi::ChangeSpeed(int speed)
+{
+	if (ready)
+	{
+		send("AT+CIOBAUD=" + speed);
+	}
+}
+
+void WiFi::Reset(int speed)
+{
+	send("AT+RST");
+	Check();
+	ChangeSpeed(speed);
+}
+
+void WiFi::CurrentLog(String name, String password, int port)
+{
+	if (ready)
+	{
+		if (!opened)
+		{
+			Open();
+		}
+		send("AT+CWSAP_CUR=" + name + "," + password + ",5,3");
+		UseTCP(port);
+	}
+}
+
+void WiFi::StaticLog(String name, String password, int port)
+{
+	if (ready)
+	{
+		if (!opened)
+		{
+			Open();
+		}
+		send("AT+CWSAP_DEF=" + name + "," + password + ",5,3");
+		UseTCP(port);
+	}
+}
+
+String WiFi::NetsList()
+{
+	if (ready)
+	{
+		if (opened)
+		{
+			Close();
+		}
+		send("AT+CWLAP");
+		return read();
+	}
+}
+
+void WiFi::Close()
+{
+	send("AT+CWMODE_CUR=1");
+	opened = false;
+}
+
+void WiFi::Open()
+{
+	send("AT+CWMODE_CUR=2");
+	opened = true;
+}
+
+void WiFi::UseTCP(int port)
+{
+	send("AT+CIPMUX=1");
+	send("AT+CIPSERVER=1," + port);
+}
+
+void WiFi::Check()
+{
+	send("AT");
+	if (read() == "OK")
+	{
+		ready = true;
+		Close();
+	}
+	else
+	{
+		ready = false;
+	}
+}
+
--- code/Arduino/connectors/WiFi.h
+++ code/Arduino/connectors/WiFi.h
@@ -6,5 +6,79 @@ class WiFi : public ConnectingDevice
 public:
 	WiFi(int rx, int tx, int speed);
 	~WiFi();
-};
+	
+	/**
+	* Проверка версии прошивки
+	* Возвращает значение типа String
+	*/
+	virtual String VersionCheck();
+	
+	/**
+	* Выводит список доступных модулю в данный момент сетей для подключения
+	* Возвращает значение типа String
+	*/
+	virtual String NetsList();
+	
+	/**
+	* Получение текущих IP и MAC
+	* Возвращает значение типа String
+	*/
+	virtual String CheckIPandMAC();
 
+	/**
+	* Изменяет рабочую скорость модуля
+	* Принимает новую скорость работы модуля
+	*/
+	virtual void ChangeSpeed(int speed);
+	
+	/**
+	* Перезапускает модуль
+	* Принимает новую скорость работы модуля
+	*/
+	virtual void Reset(int speed);
+	
+	/**
+	* Делает первичную проверку модуля
+	*/
+	virtual void Check();
+	
+	/**
+	* Переводит модуль в режим клиента
+	*/
+	virtual void Close();
+	
+	/**
+	* Переводит модуль в режим хоста без TCP
+	*/
+	virtual void Open();
+	
+	/**
+	* Включает TCP
+	* Принимает номер порта типа int
+	*/
+	virtual void UseTCP(int port);
+	
+	/**
+	* Устанавливает параметры хоста без записи в кэш
+	* Принимает имя точки доступа типа String, пароль типа String и номер порта типа int для вызова функции UseTCP
+	*/
+	virtual void CurrentLog(String name, String password, int port);
+	
+	/**
+	* Устанавливает параметры хоста с записью в кэш
+	* Принимает имя точки доступа типа String, пароль типа String и номер порта типа int для вызова функции UseTCP
+	*/
+	virtual void StaticLog(String name, String password, int port);
+
+private:
+	
+	/**
+	* Индикатор готовности модуля к работе
+	*/
+	bool ready = false;
+
+	/**
+	* Индикатор состояния модуля клиент/хост
+	*/
+	bool opened = false;
+};
