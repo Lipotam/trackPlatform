c11dce493921ab8b249a4034e65656d03bc764c3
AJIOB <aleksandr9809@gmail.com>
2017-08-15 09:54:45 +0300

    * Поправлена компиляция исключений под ОС linux

--- code/c++/cxx-api/TrackPlatformException.h
+++ code/c++/cxx-api/TrackPlatformException.h
@@ -6,9 +6,17 @@
 
 class TrackPlatformException : public std::exception
 {
+#ifndef _WIN32
+    std::string errorText;
+#endif /* _WIN32 */
+
 public:
 	TrackPlatformException();
 	explicit TrackPlatformException(const std::string& errorMessage);
+
+#ifndef _WIN32
+    const char* what() const noexcept override;
+#endif /* _WIN32 */
 };
 
 #endif /* _TRACK_PLATFORM_EXCEPTION_H_ */
