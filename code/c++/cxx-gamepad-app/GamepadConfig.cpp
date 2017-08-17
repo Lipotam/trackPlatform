0c58a0a21232a8fa2d8271917ac6164973e515bd
AJIOB <aleksandr9809@gmail.com>
2017-08-18 00:09:25 +0300

    Описана базовая конфигурация геймпада и способы работы с ней (получение текущего значения конфигурации)

--- code/c++/cxx-gamepad-app/GamepadConfig.cpp
+++ code/c++/cxx-gamepad-app/GamepadConfig.cpp
@@ -1,1 +1,39 @@
 ﻿#include "GamepadConfig.h"
+
+GamepadConfig::GamepadConfig() : 
+	buttonsBinding {
+		GamepadCommands::distanceSensorRefresh,        /* 1 - A */
+		GamepadCommands::exit,                         /* 2 - B */
+		GamepadCommands::lineSensorRefresh,            /* 3 - X */
+		GamepadCommands::none,                         /* 4 - Y */
+		GamepadCommands::none,                         /* 5 */
+		GamepadCommands::none,                         /* 6 */
+		GamepadCommands::none,                         /* 7 */
+		GamepadCommands::none,                         /* 8 */
+		GamepadCommands::none,                         /* 9 */
+		GamepadCommands::none,                         /* 10 */
+		GamepadCommands::none,                         /* 11 */
+		GamepadCommands::none,                         /* 12 */
+		GamepadCommands::none,                         /* 13 */
+		GamepadCommands::none,                         /* 14 */
+	}, 
+	sticksBinding {
+		GamepadCommands::moveRobotX,                   /* 1 - left X */
+		GamepadCommands::moveRobotY,                   /* 2 - left Y */
+		GamepadCommands::moveCameraX,                  /* 3 - right X */
+		GamepadCommands::moveCameraY,                  /* 4 - right Y */
+		GamepadCommands::none,                         /* 5 */
+		GamepadCommands::none,                         /* 6 */
+	}
+{
+}
+
+GamepadCommands GamepadConfig::button(int index) const
+{
+    return ((index >= 0) && (index < maxButtons)) ? buttonsBinding[index] : GamepadCommands::none;
+}
+
+GamepadCommands GamepadConfig::stick(int index) const
+{
+    return ((index >= 0) && (index < maxSticks)) ? sticksBinding[index] : GamepadCommands::none;
+}
--- code/c++/cxx-gamepad-app/GamepadConfig.h
+++ code/c++/cxx-gamepad-app/GamepadConfig.h
@@ -1,14 +1,42 @@
 ﻿#ifndef _GAMEPAD_CONFIG_H_
 #define _GAMEPAD_CONFIG_H_
 
-typedef enum
+enum class GamepadCommands
 {
+	none,
+
+	/* App management */
+	exit,
+
+	/* Movement */
+	moveRobotX,
+	moveRobotY,
+	moveCameraX,
+	moveCameraY,
 	
-} GamepadCommands;
+	/* Sensors */
+	lineSensorRefresh,
+	distanceSensorRefresh,
+};
 
 class GamepadConfig
 {
+	static const short int maxButtons = 14;
+	static const short int maxSticks = 6;
+
+	GamepadCommands buttonsBinding[maxButtons];
+	GamepadCommands sticksBinding[maxSticks];
 public:
+	GamepadConfig();
+
+	/**
+	 * @return Button mapping command or @GamepadCommands::none, if not exists
+	 */
+	GamepadCommands button(int index) const;
+	/**
+	 * @return Button mapping command or @GamepadCommands::none, if not exists
+	 */
+	GamepadCommands stick(int index) const;
 };
 
 #endif /* _GAMEPAD_CONFIG_H_ */
