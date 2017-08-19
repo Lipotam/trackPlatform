3f3baac18ae40bfaa42b29b8e96c986ae5ee44c1
AJIOB <aleksandr9809@gmail.com>
2017-08-19 22:32:57 +0300

    Добавлена возможность логирования в файл информации (по умолчанию выключена)

--- code/c++/cxx-gamepad-app/main.cpp
+++ code/c++/cxx-gamepad-app/main.cpp
@@ -4,6 +4,7 @@
 #include "GamepadManager.h"
 #include "ConnectionModes.h"
 #include "TrackPlatform_Manager.h"
+#include "Logger.h"
 
 int main(int argc, char* argv[])
 {
@@ -11,6 +12,8 @@ int main(int argc, char* argv[])
 	uint32_t baudrate = 9600U;
 
 	std::cout << "rx = tx = " << rtx << "; baudrate = " << baudrate << std::endl;
+
+	Logger::log("Add is running");
 
 	try
 	{
@@ -22,9 +25,11 @@ int main(int argc, char* argv[])
 	catch(...)
 	{
 		std::cout << "Exception was catched" << std::endl;
+		Logger::log("Exception was catched");
 		return 1;
 	}
 
+	Logger::log("App is closed");
 	return 0;
 }
 
