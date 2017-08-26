9e853a6b5644a4e218a17c4172dbe6219abb1656
AJIOB <aleksandr9809@gmail.com>
2017-08-26 23:50:47 +0300

    Добавлена совместимость с API v2. Добавлен деструктор (на всякий случай)

--- code/Arduino/peripheral/ConnectionController.h
+++ code/Arduino/peripheral/ConnectionController.h
@@ -2,6 +2,7 @@
 #include "../connectors/USB.h"
 #include "../connectors/Bluetooth.h"
 #include "../connectors/WiFi.h"
+#include "../CommandsEnum.h"
 
 class ConnectionController
 {
@@ -12,11 +13,15 @@ class ConnectionController
 
 	static const char connectCommand[];
 	static const char disconnectCommand[];
+	static const StartCommands lowestAPI = startBasicAPI;
+	static const StartCommands highestAPI = APIWithAnswer;
 
 	bool isConnected = false;
+	StartCommands connectedAPIversion;
 
 public:
 	ConnectionController();
+	~ConnectionController();
 
 	void waitForConnection();
 	ConnectingDevice* getDevice() const;
