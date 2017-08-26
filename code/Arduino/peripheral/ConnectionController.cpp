11b40bc00285c4432cb105150033c9da56401909
AJIOB <aleksandr9809@gmail.com>
2017-08-26 22:30:48 +0300

    Работа с соединениями вынесена в отдельный класс.

---
+++ code/Arduino/peripheral/ConnectionController.cpp
@@ -0,0 +1,77 @@
+﻿#include "../Constants.h"
+#include "../CommandsEnum.h"
+#include "../connectors/DebugSerial.h"
+
+#include "ConnectionController.h"
+
+const char ConnectionController::connectCommand[] = { communicationControllerID, startCommunicationCommand, startBasicAPI, 0 };
+const char ConnectionController::disconnectCommand[] = { communicationControllerID, stopCommunicationCommand, 0 };
+
+ConnectionController::ConnectionController()
+{
+	usb = new USB(Constants::usb_serial_speed);
+	wifi = new WiFi(Constants::wifi_serial_speed);
+	bluetooth = new Bluetooth(Constants::bluetooth_serial_speed);
+}
+
+void ConnectionController::waitForConnection()
+{
+	DEBUG_PRINTLN("Arduino tries to found a manager");
+	while (!isConnected) {
+		if (bluetooth->isActive() && bluetooth->read() == connectCommand) {
+			isConnected = true;
+			device = bluetooth;
+			DEBUG_PRINTLN("Bluetooth");
+		}
+		else if (wifi->isActive()) {
+		isConnected = true;
+		device = wifi;
+		DEBUG_PRINTLN("Wifi");
+		}
+		else if (usb->isActive()) {
+			isConnected = true;
+			device = usb;
+			DEBUG_PRINTLN("USB");
+		}
+	}
+
+	device->send("OK");
+	DEBUG_PRINTLN("Arduino found a manager");
+}
+
+ConnectingDevice* ConnectionController::getDevice() const
+{
+	return device;
+}
+
+String ConnectionController::getCommand()
+{
+	if (!isConnected)
+	{
+		return "";
+	}
+
+	String command;
+	do
+	{
+		while (!device->isActive())
+		{
+			delay(1);
+		}
+
+		command = device->read();
+
+		//debug
+		DEBUG_PRINT("Command: ");
+		DEBUG_PRINTLNHEX(command);
+
+		if (command != disconnectCommand)
+		{
+			break;
+		}
+
+		isConnected = false;
+		waitForConnection();
+	} while (true);
+	return command;
+}
---
+++ code/Arduino/peripheral/ConnectionController.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include "../connectors/USB.h"
+#include "../connectors/Bluetooth.h"
+#include "../connectors/WiFi.h"
+
+class ConnectionController
+{
+	USB* usb = nullptr;
+	Bluetooth* bluetooth = nullptr;
+	WiFi* wifi = nullptr;
+	ConnectingDevice *device = nullptr;
+
+	static const char connectCommand[];
+	static const char disconnectCommand[];
+
+	bool isConnected = false;
+
+public:
+	ConnectionController();
+
+	void waitForConnection();
+	ConnectingDevice* getDevice() const;
+	String getCommand();
+};
