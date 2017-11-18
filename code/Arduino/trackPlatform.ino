c8daa63bc05191d9e00d21652757e6c81c32a48a
AJIOB <aleksandr9809@gmail.com>
2017-11-18 18:12:05 +0300

    Remove LED debug

--- code/Arduino/trackPlatform.ino
+++ code/Arduino/trackPlatform.ino
@@ -3,28 +3,12 @@
 #include "connectors/DebugSerial.h"
 #include "peripheral/ConnectionController.h"
 
-#define DIOD_DEBUG
-
-#ifdef DIOD_DEBUG
-
-void diodInit();
-void on43();
-void off43();
-void on45();
-void off45();
-
-#endif /* DIOD_DEBUG */
-
 Constants constants;
 ConnectionController* connector;
 CommandsController* controller = nullptr;
 
 void setup()
 {
-#ifdef DIOD_DEBUG
-	diodInit();
-#endif /* DIOD_DEBUG */
-
 	controller = new CommandsController();
 	connector = new ConnectionController();
 
@@ -45,33 +29,3 @@ void loop()
 		DEBUG_PRINTLN("Command is short");
 	}
 }
-
-#ifdef DIOD_DEBUG
-
-void diodInit()
-{
-	pinMode(43, OUTPUT);
-	pinMode(45, OUTPUT);
-}
-
-void on43()
-{
-	digitalWrite(43, HIGH);
-}
-
-void off43()
-{
-	digitalWrite(43, LOW);
-}
-
-void on45()
-{
-	digitalWrite(45, HIGH);
-}
-
-void off45()
-{
-	digitalWrite(45, LOW);
-}
-
-#endif /* DIOD_DEBUG */
