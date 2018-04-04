f5a3cf3a056ed1eec3fc045c6ac974b19d69dd81
AJIOB <aleksandr9809@gmail.com>
2018-04-04 23:53:17 +0300

    Crc tests were rewritten to NUnit Framework

--- code/cs_api/Tests/CrcTests.cs
+++ code/cs_api/Tests/CrcTests.cs
@@ -1,19 +1,29 @@
 ﻿using System;
 using System.Text;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
 using TrackPlatform.Tools;
 
 namespace Tests
 {
-    [TestClass]
+    [TestFixture]
     public class CrcTests
     {
-        [TestMethod]
+        [Test]
         public void Seq123456789()
         {
             string input = "123456789";
             ushort answer = 0x4B37;
-            Assert.IsTrue(Crc16.Modbus(Encoding.ASCII.GetBytes(input)) == answer);
+            AnySeq(input, answer);
+        }
+
+        [TestCase("1", 0x947E)]
+        [TestCase("10", 0x3414)]
+        [TestCase("123456789", 0x4B37)]
+        [TestCase("FEEDCAFE", 0x4C98)]
+        public void AnySeq(string input, int answer)
+        {
+            ushort res = Crc16.Modbus(Encoding.ASCII.GetBytes(input));
+            Assert.IsTrue(res == answer, $"Message \"{input}\", res: {res:X4}, required: {answer:X4}");
         }
     }
 }
