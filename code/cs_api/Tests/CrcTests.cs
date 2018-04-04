9bd6efc260c2c343013acccf73d8bc5712dcdc9b
AJIOB <aleksandr9809@gmail.com>
2018-04-04 23:25:24 +0300

    C# API CRC test was added

---
+++ code/cs_api/Tests/CrcTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrackPlatform.Tools;
+
+namespace Tests
+{
+    [TestClass]
+    public class CrcTests
+    {
+        [TestMethod]
+        public void Seq123456789()
+        {
+            string input = "123456789";
+            ushort answer = 0x4B37;
+            Assert.IsTrue(Crc16.Modbus(Encoding.ASCII.GetBytes(input)) == answer);
+        }
+    }
+}
