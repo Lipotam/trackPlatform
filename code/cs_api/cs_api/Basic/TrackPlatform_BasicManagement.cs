932ad1ffab34db4fd18bd6dcf26bc0e15b90f3f5
AJIOB <aleksandr9809@gmail.com>
2018-04-05 20:33:58 +0300

    C# API: basic management rewriting

--- code/cs_api/cs_api/Basic/TrackPlatform_BasicManagement.cs
+++ code/cs_api/cs_api/Basic/TrackPlatform_BasicManagement.cs
@@ -1,267 +1,271 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using TrackPlatform.Tools;
 
-public class TrackPlatform_BasicManagement : System.IDisposable
+public class TrackPlatform_BasicManagement
 {
-	private TrackPlatform_BasicConnector connector;
+    public TrackPlatform_BasicConnector connector { get; private set; }
 
-	protected const uint8_t minSpeed = 0;
-	protected const uint8_t maxSpeed = 255;
-	protected const uint8_t minServoAngle = 0;
-	protected const uint8_t maxServoAngle = 180;
-	protected const sbyte delimiter = (sbyte)';';
-	protected readonly double minInputSpeed = -1;
-	protected readonly double maxInputSpeed = 1;
+    protected const byte minSpeed = 0;
+    protected const byte maxSpeed = 255;
+    protected const byte minServoAngle = 0;
+    protected const byte maxServoAngle = 180;
+    protected const byte delimiter = (byte)';';
+    protected const double minInputSpeed = -1;
+    protected const double maxInputSpeed = 1;
 
-	protected string sendCommand(ControllerEnum targetController, string additionalInfo, bool isWaitAnswer = false)
-	{
-		return connector.sendOneCommand((sbyte)targetController + additionalInfo, isWaitAnswer);
-	}
+    protected int CalcSpeed(double speed)
+    {
+        return (int) (speed * (maxSpeed - minSpeed) + minSpeed);
+    }
 
-	protected static List<uint32_t> parseStringToArray(string s)
-	{
-		List<uint32_t> distancies = new List<uint32_t>();
-		size_t stringOldLen = 0;
-		size_t posToNextValue = 0;
-		try
-		{
-			do
-			{
+    protected byte[] sendCommand(ControllerEnum targetController, byte[] additionalInfo, bool isWaitAnswer = false)
+    {
+        byte[] buff = additionalInfo.Add((byte)targetController, 0);
+        return connector.sendOneCommand(buff, isWaitAnswer);
+    }
 
-				distancies.Add(std::stoul(s, posToNextValue));
-				stringOldLen = s.Length;
-				s = s.Substring(posToNextValue + sizeof (sbyte));
+    protected static List<uint> parseStringToArray(byte[] s)
+    {
+        List<uint> distancies = new List<uint>();
+        int stringOldLen = 0;
+        int posToNextValue = 0;
+        try
+        {
+            do
+            {
+                //TODO: parse 
+                distancies.Add(std::stoul(s, posToNextValue));
+                stringOldLen = s.Length;
+                s = s.Substring(posToNextValue + sizeof(sbyte));
 
-			} while ((s != "") && (stringOldLen > s.Length));
-		}
-		catch
-		{
-			return distancies;
-		}
+            } while ((s != "") && (stringOldLen > s.Length));
+        }
+        catch
+        {
+            return distancies;
+        }
 
-		if (stringOldLen <= s.Length)
-		{
-			distancies.RemoveAt(distancies.Count - 1);
-		}
-		return distancies;
-	}
+        if (stringOldLen <= s.Length)
+        {
+            distancies.RemoveAt(distancies.Count - 1);
+        }
+        return distancies;
+    }
 
-//C++ TO C# CONVERTER WARNING: 'const' methods are not available in C#:
-//ORIGINAL LINE: TrackPlatform_BasicConnector* getConnector() const
-	protected TrackPlatform_BasicConnector getConnector()
-	{
-		return connector;
-	}
-
-	/**
+    /**
 	 * \brief Send movement command to with connector
 	 * \param command Command to send with speed
 	 * \param speed Speed from @minSpeed to @maxSpeed interval
 	 * \return true, if all is good, else false
 	 */
-	protected bool sendMovement(MoveEnum command, double speed)
-	{
-		if (speed < minInputSpeed || speed > maxInputSpeed)
-		{
-			return false;
-		}
+    protected bool sendMovement(MoveEnum command, double speed)
+    {
+        if (speed < minInputSpeed || speed > maxInputSpeed)
+        {
+            return false;
+        }
 
-		string toSend = new string(1, command);
-		toSend += Convert.ToString((uint16_t)(speed * (maxSpeed - minSpeed) + minSpeed));
-		sendCommand(ControllerEnum.movementControllerID, toSend);
+        byte[] buff = CalcSpeed(speed).NumToArray().Add((byte)command, 0);
+        sendCommand(ControllerEnum.movementControllerID, buff);
 
-		return true;
-	}
+        return true;
+    }
 
-	public TrackPlatform_BasicManagement(TrackPlatform_BasicConnector connector)
-	{
-		this.connector = connector;
-	}
-	public virtual void Dispose()
-	{
-	}
+    public TrackPlatform_BasicManagement(TrackPlatform_BasicConnector connector)
+    {
+        this.connector = connector;
+    }
 
-	/* movement controller */
-	public void moveForward()
-	{
-		moveForward(maxInputSpeed);
-	}
-	//speed must be in [0,1] range
-	public bool moveForward(double speed)
-	{
-		return sendMovement(MoveEnum.forward_speed, speed);
-	}
-	public void moveForward(uint32_t timeInMSec)
-	{
-		moveForward(maxInputSpeed);
-		std::this_thread.sleep_for(std::chrono.milliseconds(timeInMSec));
-		moveStopAll();
-	}
+    /* movement controller */
+    public void moveForward()
+    {
+        moveForward(maxInputSpeed);
+    }
+    //speed must be in [0,1] range
+    public bool moveForward(double speed)
+    {
+        return sendMovement(MoveEnum.forward_speed, speed);
+    }
+    public void moveForward(int timeInMSec)
+    {
+        moveForward(maxInputSpeed);
+        Thread.Sleep(timeInMSec);
+        moveStopAll();
+    }
 
-	public void moveBackward()
-	{
-		moveForward(-maxInputSpeed);
-	}
-	//speed must be in [0,1] range
-	public bool moveBackward(double speed)
-	{
-		return moveForward(-speed);
-	}
+    public void moveBackward()
+    {
+        moveForward(-maxInputSpeed);
+    }
+    //speed must be in [0,1] range
+    public bool moveBackward(double speed)
+    {
+        return moveForward(-speed);
+    }
 
-	public bool rotateClockwise(double speed = maxInputSpeed)
-	{
-		return sendMovement(MoveEnum.clockwise, speed);
-	}
-	public bool rotateAntiClockwise(double speed = maxInputSpeed)
-	{
-		return rotateClockwise(-speed);
-	}
+    public bool rotateClockwise(double speed = maxInputSpeed)
+    {
+        return sendMovement(MoveEnum.clockwise, speed);
+    }
+    public bool rotateAntiClockwise(double speed = maxInputSpeed)
+    {
+        return rotateClockwise(-speed);
+    }
 
-	public void moveStopAll()
-	{
-		string toSend = new string(1, MoveEnum.stop);
-		sendCommand(ControllerEnum.movementControllerID, toSend);
-	}
+    public void moveStopAll()
+    {
+        sendCommand(ControllerEnum.movementControllerID, new[] { (byte)MoveEnum.stop });
+    }
 
-	//speed must be in [-1, 1] range
-	public bool setTrackForwardSpeed(TrackIndex trackId, double speed)
-	{
-		if (speed < -1 || speed > 1)
-		{
-			return false;
-		}
+    //speed must be in [-1, 1] range
+    public bool setTrackForwardSpeed(TrackIndex trackId, double speed)
+    {
+        if (speed < minInputSpeed || speed > maxInputSpeed)
+        {
+            return false;
+        }
 
-		string toSend = new string(1, MoveEnum.track_set_speed);
-//C++ TO C# CONVERTER TODO TASK: There is no equivalent to 'reinterpret_cast' in C#:
-		toSend += Convert.ToString(*reinterpret_cast<const uint8_t>(trackId));
-		toSend += (char)delimiter;
-		toSend += Convert.ToString((int16_t)(speed * (maxSpeed - minSpeed) + minSpeed));
-		sendCommand(ControllerEnum.movementControllerID, toSend);
+        byte[] buff =
+        {
+            (byte) MoveEnum.track_set_speed,
+            (byte) (trackId + '0'),
+            delimiter,
+        };
 
-		return true;
-	}
-	//speed must be in [-1, 1] range
-	public bool setAllTrackForwardSpeed(double leftSpeed, double rightSpeed)
-	{
-		if (leftSpeed < -1 || leftSpeed > 1 || rightSpeed < -1 || rightSpeed > 1)
-		{
-			return false;
-		}
+        buff = buff.Concat(CalcSpeed(speed).NumToArray());
+        sendCommand(ControllerEnum.movementControllerID, buff);
 
-		string toSend = new string(1, MoveEnum.track_all_set_speed);
-		toSend += Convert.ToString((int16_t)(leftSpeed * (maxSpeed - minSpeed) + minSpeed));
-		toSend += (char)delimiter;
-		toSend += Convert.ToString((int16_t)(rightSpeed * (maxSpeed - minSpeed) + minSpeed));
-		sendCommand(ControllerEnum.movementControllerID, toSend);
+        return true;
+    }
+    //speed must be in [-1, 1] range
+    public bool setAllTrackForwardSpeed(double leftSpeed, double rightSpeed)
+    {
+        if (leftSpeed < minInputSpeed || leftSpeed > maxInputSpeed || rightSpeed < minInputSpeed || rightSpeed > maxInputSpeed)
+        {
+            return false;
+        }
 
-		return true;
-	}
+        byte[] buff =
+        {
+            (byte) MoveEnum.track_all_set_speed,
+        };
 
-	/* sensors controller */
-	public uint32_t sensorDistanceGetValue(uint8_t num)
-	{
-		string toSend = new string(1, SensorsEnum.distance_sensor);
-		toSend += Convert.ToString(num);
-		string answer = sendCommand(ControllerEnum.sensorsControllerID, toSend, true);
-		return Convert.ToInt32(answer);
-	}
-	public List<uint32_t> sensorDistanceGetAllValues()
-	{
-		string toSend = new string(1, SensorsEnum.distance_sensor_all);
-		string answer = sendCommand(ControllerEnum.sensorsControllerID, toSend, true);
-		return parseStringToArray(answer);
-	}
-	public uint32_t sensorLineGetValue(uint8_t num)
-	{
-		string toSend = new string(1, SensorsEnum.line_sensor);
-		toSend += Convert.ToString(num);
-		string answer = sendCommand(ControllerEnum.sensorsControllerID, toSend, true);
-		return Convert.ToInt32(answer);
-	}
-	public List<uint32_t> sensorLineGetAllValues()
-	{
-		string toSend = new string(1, SensorsEnum.line_sensor_all);
-		string answer = sendCommand(ControllerEnum.sensorsControllerID, toSend, true);
-		return parseStringToArray(answer);
-	}
-	public uint32_t sensorDistanceGetRawValue(uint8_t num)
-	{
-		string toSend = new string(1, SensorsEnum.raw_distance_sensor);
-		toSend += Convert.ToString(num);
-		string answer = sendCommand(ControllerEnum.sensorsControllerID, toSend, true);
-		return Convert.ToInt32(answer);
-	}
-	public List<uint32_t> sensorDistanceGetAllRawValues()
-	{
-		string toSend = new string(1, SensorsEnum.raw_distance_sensor_all);
-		string answer = sendCommand(ControllerEnum.sensorsControllerID, toSend, true);
-		return parseStringToArray(answer);
-	}
-	public uint32_t sensorLineGetRawValue(uint8_t num)
-	{
-		string toSend = new string(1, SensorsEnum.raw_line_sensor);
-		toSend += Convert.ToString(num);
-		string answer = sendCommand(ControllerEnum.sensorsControllerID, toSend, true);
-		return Convert.ToInt32(answer);
-	}
-	public List<uint32_t> sensorLineGetAllRawValues()
-	{
-		string toSend = new string(1, SensorsEnum.raw_line_sensor_all);
-		string answer = sendCommand(ControllerEnum.sensorsControllerID, toSend, true);
-		return parseStringToArray(answer);
-	}
+        buff = buff.Concat(CalcSpeed(leftSpeed).NumToArray()).Concat(delimiter);
+        buff = buff.Concat(CalcSpeed(rightSpeed).NumToArray());
 
-	/* servo controller */
-	public void servoSetHorizontalAngle(uint16_t angle)
-	{
-//C++ TO C# CONVERTER TODO TASK: The following line was determined to contain a copy constructor call - this should be verified and a copy constructor should be created:
-//ORIGINAL LINE: servoSetAngle(xy_plane, angle);
-		servoSetAngle(ServoIndex.xy_plane, new uint16_t(angle));
-	}
-	public void servoSetVerticalAngle(uint16_t angle)
-	{
-//C++ TO C# CONVERTER TODO TASK: The following line was determined to contain a copy constructor call - this should be verified and a copy constructor should be created:
-//ORIGINAL LINE: servoSetAngle(xz_plane, angle);
-		servoSetAngle(ServoIndex.xz_plane, new uint16_t(angle));
-	}
-	public void servoSetHorizontalVerticalAngle(uint16_t horizontalAngle, uint16_t verticalAngle)
-	{
-//C++ TO C# CONVERTER TODO TASK: The following line was determined to contain a copy constructor call - this should be verified and a copy constructor should be created:
-//ORIGINAL LINE: servoSetAngle(xy_plane, horizontalAngle);
-		servoSetAngle(ServoIndex.xy_plane, new uint16_t(horizontalAngle));
-//C++ TO C# CONVERTER TODO TASK: The following line was determined to contain a copy constructor call - this should be verified and a copy constructor should be created:
-//ORIGINAL LINE: servoSetAngle(xz_plane, verticalAngle);
-		servoSetAngle(ServoIndex.xz_plane, new uint16_t(verticalAngle));
-	}
-	public List<uint32_t> servoGetAngles()
-	{
-		List<uint32_t> answer = new List<uint32_t>();
-		answer.Add(servoGetAngle(ServoIndex.xy_plane));
-		answer.Add(servoGetAngle(ServoIndex.xz_plane));
-		return answer;
-	}
-	//angle must be in [0, 180] range
-	public bool servoSetAngle(ServoIndex axisIndex, uint16_t angle)
-	{
-		if (angle < minServoAngle || angle > maxServoAngle)
-		{
-			return false;
-		}
+        sendCommand(ControllerEnum.movementControllerID, buff);
 
-		string toSend = new string(1, ServoCommands.set_angle);
-//C++ TO C# CONVERTER TODO TASK: There is no equivalent to 'reinterpret_cast' in C#:
-		toSend += Convert.ToString(*reinterpret_cast<const uint8_t>(axisIndex));
-		toSend += (char)delimiter;
-		toSend += Convert.ToString(angle);
-		sendCommand(ControllerEnum.servoControllerID, toSend);
-		return true;
-	}
-	public uint16_t servoGetAngle(ServoIndex axisIndex)
-	{
-		string toSend = new string(1, ServoCommands.get_angle);
-//C++ TO C# CONVERTER TODO TASK: There is no equivalent to 'reinterpret_cast' in C#:
-		toSend += Convert.ToString(*reinterpret_cast<const uint8_t>(axisIndex));
-		string answer = sendCommand(ControllerEnum.servoControllerID, toSend, true);
-		return Convert.ToInt32(answer);
-	}
+        return true;
+    }
+
+    /* sensors controller */
+    public uint32_t sensorDistanceGetValue(uint8_t num)
+    {
+        string toSend = new string(1, SensorsEnum.distance_sensor);
+        toSend += Convert.ToString(num);
+        string answer = sendCommand(ControllerEnum.sensorsControllerID, toSend, true);
+        return Convert.ToInt32(answer);
+    }
+    public List<uint32_t> sensorDistanceGetAllValues()
+    {
+        string toSend = new string(1, SensorsEnum.distance_sensor_all);
+        string answer = sendCommand(ControllerEnum.sensorsControllerID, toSend, true);
+        return parseStringToArray(answer);
+    }
+    public uint32_t sensorLineGetValue(uint8_t num)
+    {
+        string toSend = new string(1, SensorsEnum.line_sensor);
+        toSend += Convert.ToString(num);
+        string answer = sendCommand(ControllerEnum.sensorsControllerID, toSend, true);
+        return Convert.ToInt32(answer);
+    }
+    public List<uint32_t> sensorLineGetAllValues()
+    {
+        string toSend = new string(1, SensorsEnum.line_sensor_all);
+        string answer = sendCommand(ControllerEnum.sensorsControllerID, toSend, true);
+        return parseStringToArray(answer);
+    }
+    public uint32_t sensorDistanceGetRawValue(uint8_t num)
+    {
+        string toSend = new string(1, SensorsEnum.raw_distance_sensor);
+        toSend += Convert.ToString(num);
+        string answer = sendCommand(ControllerEnum.sensorsControllerID, toSend, true);
+        return Convert.ToInt32(answer);
+    }
+    public List<uint32_t> sensorDistanceGetAllRawValues()
+    {
+        string toSend = new string(1, SensorsEnum.raw_distance_sensor_all);
+        string answer = sendCommand(ControllerEnum.sensorsControllerID, toSend, true);
+        return parseStringToArray(answer);
+    }
+    public uint32_t sensorLineGetRawValue(uint8_t num)
+    {
+        string toSend = new string(1, SensorsEnum.raw_line_sensor);
+        toSend += Convert.ToString(num);
+        string answer = sendCommand(ControllerEnum.sensorsControllerID, toSend, true);
+        return Convert.ToInt32(answer);
+    }
+    public List<uint32_t> sensorLineGetAllRawValues()
+    {
+        string toSend = new string(1, SensorsEnum.raw_line_sensor_all);
+        string answer = sendCommand(ControllerEnum.sensorsControllerID, toSend, true);
+        return parseStringToArray(answer);
+    }
+
+    /* servo controller */
+    public void servoSetHorizontalAngle(uint16_t angle)
+    {
+        //C++ TO C# CONVERTER TODO TASK: The following line was determined to contain a copy constructor call - this should be verified and a copy constructor should be created:
+        //ORIGINAL LINE: servoSetAngle(xy_plane, angle);
+        servoSetAngle(ServoIndex.xy_plane, new uint16_t(angle));
+    }
+    public void servoSetVerticalAngle(uint16_t angle)
+    {
+        //C++ TO C# CONVERTER TODO TASK: The following line was determined to contain a copy constructor call - this should be verified and a copy constructor should be created:
+        //ORIGINAL LINE: servoSetAngle(xz_plane, angle);
+        servoSetAngle(ServoIndex.xz_plane, new uint16_t(angle));
+    }
+    public void servoSetHorizontalVerticalAngle(uint16_t horizontalAngle, uint16_t verticalAngle)
+    {
+        //C++ TO C# CONVERTER TODO TASK: The following line was determined to contain a copy constructor call - this should be verified and a copy constructor should be created:
+        //ORIGINAL LINE: servoSetAngle(xy_plane, horizontalAngle);
+        servoSetAngle(ServoIndex.xy_plane, new uint16_t(horizontalAngle));
+        //C++ TO C# CONVERTER TODO TASK: The following line was determined to contain a copy constructor call - this should be verified and a copy constructor should be created:
+        //ORIGINAL LINE: servoSetAngle(xz_plane, verticalAngle);
+        servoSetAngle(ServoIndex.xz_plane, new uint16_t(verticalAngle));
+    }
+    public List<uint32_t> servoGetAngles()
+    {
+        List<uint32_t> answer = new List<uint32_t>();
+        answer.Add(servoGetAngle(ServoIndex.xy_plane));
+        answer.Add(servoGetAngle(ServoIndex.xz_plane));
+        return answer;
+    }
+    //angle must be in [0, 180] range
+    public bool servoSetAngle(ServoIndex axisIndex, uint16_t angle)
+    {
+        if (angle < minServoAngle || angle > maxServoAngle)
+        {
+            return false;
+        }
+
+        string toSend = new string(1, ServoCommands.set_angle);
+        //C++ TO C# CONVERTER TODO TASK: There is no equivalent to 'reinterpret_cast' in C#:
+        toSend += Convert.ToString(*reinterpret_cast <const uint8_t> (axisIndex));
+        toSend += (char)delimiter;
+        toSend += Convert.ToString(angle);
+        sendCommand(ControllerEnum.servoControllerID, toSend);
+        return true;
+    }
+    public uint16_t servoGetAngle(ServoIndex axisIndex)
+    {
+        string toSend = new string(1, ServoCommands.get_angle);
+        //C++ TO C# CONVERTER TODO TASK: There is no equivalent to 'reinterpret_cast' in C#:
+        toSend += Convert.ToString(*reinterpret_cast <const uint8_t> (axisIndex));
+        string answer = sendCommand(ControllerEnum.servoControllerID, toSend, true);
+        return Convert.ToInt32(answer);
+    }
 }
--- code/cs_api/cs_api/Tools/ArrayTools.cs
+++ code/cs_api/cs_api/Tools/ArrayTools.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace TrackPlatform.Tools
 {
@@ -9,6 +10,39 @@ namespace TrackPlatform.Tools
             T[] result = new T[length];
             Array.Copy(data, index, result, 0, length);
             return result;
+        }
+        public static T[] Add<T>(this T[] data, T elem, int pos)
+        {
+            T[] result = new T[data.Length + 1];
+            Array.Copy(data, 0, result, 0, pos);
+            Array.Copy(data, pos, result, pos + 1, data.Length - pos);
+            data[pos] = elem;
+            return result;
+        }
+
+        public static T[] Add<T>(this T[] data, T[] right, int pos)
+        {
+            T[] result = new T[data.Length + right.Length];
+            Array.Copy(data, 0, result, 0, pos);
+            Array.Copy(right, 0, result, 0, right.Length);
+            Array.Copy(data, pos, result, pos + right.Length, data.Length - pos);
+            return result;
+        }
+
+        public static T[] Concat<T>(this T[] data, T right)
+        {
+            return data.Add(right, data.Length);
+        }
+
+        public static T[] Concat<T>(this T[] data, T[] right)
+        {
+            return data.Add(right, data.Length);
+        }
+
+        public static byte[] NumToArray<T>(this T num)
+        {
+            string speedString = Convert.ToString(num);
+            return Encoding.ASCII.GetBytes(speedString);
         }
     }
 }
