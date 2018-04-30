b9d8de7cc6d238cad01f2f04afb5b67f50292919
AJIOB <aleksandr9809@gmail.com>
2018-04-30 11:54:35 +0300

    C# API moving

--- code/cs_api/cs_api/CommandsSpecification/CommunicationCommands.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TrackPlatform.CommandsSpecification
-{
-    public enum CommunicationCommands
-    {
-        Start = 1,                  //starting communication command
-        Stop = 2,                   //stopping communication command
-        RefreshConnection = 3,      //refreshing connection timer communication command (since API v3)
-    }
-}
